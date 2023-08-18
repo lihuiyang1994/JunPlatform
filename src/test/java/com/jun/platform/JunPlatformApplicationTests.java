4d9f40a954817486d93f80946766ef8aef40dd0c
lihuiyang <1808686485@qq.com>
2023-08-18 22:40:23 +0800

    描述:第一次提交 版本:v1

---
+++ src/test/java/com/jun/platform/JunPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
